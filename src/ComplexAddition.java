765864459c3704c728e43c23ea8aefe9a6568b9c
Amrita Reji <amritareji@Amritas-MacBook-Pro.local>
2026-02-06 10:56:04 +0530

    hii

--- src/ComplexAddition.java
+++ src/ComplexAddition.java
@@ -1,20 +1,20 @@
 import java.util.Scanner;
 
 class Complex {
-    double real1;
-    double real2;
-    double complex1;
-    double complex2;
-    Complex(double a, double b, double c, double d){
+    int real1;
+    int real2;
+    int complex1;
+    int complex2;
+    Complex(int a, int b, int c, int d){
             real1=a;
             real2=b;
             complex1=c;
             complex2=d;
     }
     public void AddnDisplay(){
-            double realres= real1+real2;
-            double complexres=complex1+complex2;
-            System.out.println(realres+" + "+complexres+"i");
+            int realres= real1+real2;
+            int complexres=complex1+complex2;
+            System.out.println(realres+" + "+ complexres+"i");
     }
 
     // TODO: parameterized constructor Complex(int real, int imag)
@@ -26,10 +26,10 @@ class Complex {
 public class ComplexAddition {
     public static void main(String args[]){
             Scanner sc = new Scanner(System.in);
-            double rl1=sc.nextDouble();
-            double cp1=sc.nextDouble();
-            double rl2=sc.nextDouble();
-            double cp2=sc.nextDouble();
+            int rl1=sc.nextInt();
+            int cp1=sc.nextInt();
+            int rl2=sc.nextInt();
+            int cp2=sc.nextInt();
             Complex c1 = new Complex(rl1,rl2,cp1,cp2);
             c1.AddnDisplay();
     }
